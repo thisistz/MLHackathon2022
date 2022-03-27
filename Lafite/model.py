7a7f6d96235f96e398cc3fd6cd805424b0776c2b
Luke Holman <Holman57@gmail.com>
2022-03-26 17:20:29 -0700

    stream-line process into single file

---
+++ Lafite/model.py
@@ -0,0 +1,77 @@
+import time
+
+import torch
+import numpy as np
+import pickle
+import os
+from PIL import Image as PIL_Image
+from pathlib import Path
+from tqdm import tqdm
+import dnnlib, legacy
+import clip
+import torch.nn.functional as F
+import torchvision.transforms as T
+from tqdm import tqdm
+import scipy
+import time
+
+
+class Generator:
+    def __init__(self, device, path):
+        self.name = 'generator'
+        self.model = self.load_model(device, path)
+        self.device = device
+        if device == 'cpu':
+            self.force_32 = True
+        else:
+            self.force_32 = False
+
+    def load_model(self, device, path):
+        with dnnlib.util.open_url(path) as f:
+            network = legacy.load_network_pkl(f)
+            self.G_ema = network['G_ema'].to(device)
+            self.D = network['D'].to(device)
+            #                 self.G = network['G'].to(device)
+            if device == 'cpu':
+                self.G_ema = self.G_ema.float()
+            return self.G_ema
+
+    def generate(self, z, c, fts, noise_mode='const', return_styles=True):
+        return self.model(z, c, fts=fts, noise_mode=noise_mode, return_styles=return_styles, force_fp32=self.force_32)
+
+    def generate_from_style(self, style, noise_mode='const'):
+        ws = torch.randn(1, self.model.num_ws, 512)
+        return self.model.synthesis(ws, fts=None, styles=style, noise_mode=noise_mode, force_fp32=self.force_32)
+
+    def tensor_to_img(self, tensor):
+        img = torch.clamp((tensor + 1.) * 127.5, 0., 255.)
+        img_list = img.permute(0, 2, 3, 1)
+        img_list = [img for img in img_list]
+        return PIL_Image.fromarray(torch.cat(img_list, dim=-2).detach().cpu().numpy().astype(np.uint8))
+
+change = ''
+
+while True:
+    with torch.no_grad():
+        device = 'cpu'
+        path = 'COCO2014_CLIP_ViTB32_all_text.pkl'
+        generator = Generator(device=device, path=path)
+        clip_model, _ = clip.load("ViT-B/32", device=device)
+        clip_model = clip_model.eval()
+
+        f = open("message.txt", "r")
+        txt = f.read()
+        if txt != change:
+            print(f.read())
+            tokenized_text = clip.tokenize([txt]).to(device)
+            txt_fts = clip_model.encode_text(tokenized_text)
+            txt_fts = txt_fts / txt_fts.norm(dim=-1, keepdim=True)
+
+            z = torch.randn((1, 512)).to(device)
+            c = torch.randn((1, 1)).to(device)
+            img, _ = generator.generate(z=z, c=c, fts=txt_fts)
+            to_show_img = generator.tensor_to_img(img)
+            to_show_img.save('./generated.jpg')
+            time.sleep(1)
+
+        change = txt
---
+++ realtime-transcription/README.md
@@ -0,0 +1,28 @@
+# AssemblyAI Real-Time Transcription Browser Example
+
+## Description
+This open-source repo provided by AssemblyAI displays how to use our real-time API in the browser!
+
+In this app, we grab an audio stream from the user's computer and then send that over a WebSocket to AssemblyAI for real-time transcription. Once AssemblyAI begins transcribing, we display the text in the browser. This is accomplished using Express for our backend and Vanilla JavaScript with the npm package [recordrtc](https://www.npmjs.com/package/recordrtc) for our frontend.
+
+## How To Install and Run the Project
+
+##### ❗Important❗
+
+- Before running this app, you need to upgrade your AssemblyAI account. The real-time API is only available to upgraded accounts at this time.
+- Running the app before upgrading will cause an **error with a 402 status code.** ⚠️
+- To upgrade your account you need to add a card. You can do that in your dashboard [here](https://app.assemblyai.com/)!
+
+##### Instructions
+
+1. Clone the repo to your local machine.
+2. Open a terminal in the main directory housing the project. In this case `realtime-transcription-browser-js-example`.
+3. Run `npm install` to ensure all dependencies are installed.
+4. Add your AssemblyAI key to line 13 of [`server.js`](https://github.com/AssemblyAI/realtime-transcription-browser-js-example/blob/62e07e1d2a7ee2e13349c4e817b048e41334c4ec/js/server.js#L13)
+5. Start the server with the command `npm run server` (will run on port 8000).
+7. Open a second terminal in the main directory of the project and start the client side with `npm run client` (will run on port 3000).
+
+## Further Documentation
+- [AssemblyAI Real-Time Documention](https://docs.assemblyai.com/overview/real-time-transcription)
+- [recordrtc](https://www.npmjs.com/package/recordrtc)
+- [Express](https://expressjs.com/)
